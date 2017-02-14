c9d3066aeb7e1ce9a9ecf54ed557341ea4880a11
macbook <petitm31@gmail.com>
2017-02-14 16:13:10 +0100

    Add content to Articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214150837) do
+ActiveRecord::Schema.define(version: 20170214151225) do
 
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "title"
+    t.text     "content"
   end
 
 end
